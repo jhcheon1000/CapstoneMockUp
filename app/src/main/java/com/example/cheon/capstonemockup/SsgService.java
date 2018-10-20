7f23e616caecf1d26beb1ae5138e9e15c259d040
jhcheon1000 <hhj2134@gmail.com>
2018-10-20 23:06:28 +0900

    first service class

---
+++ app/src/main/java/com/example/cheon/capstonemockup/SsgService.java
@@ -0,0 +1,34 @@
+package com.example.cheon.capstonemockup;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+public class SsgService extends Service {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
